630bee791770eb96f3b15c66bf43d7e83d911b70
Lucas-Choi-17 <csissis@naver.com>
2024-06-25 15:51:51 +0900

    Feat: "고객 적립금 조회 페이지 구현"
    임시 고객정보를 전달하여 가능한 적립금 조회
    
    Resolves: #10

--- src/main/java/com/ohgiraffers/notimplement/user/controller/UserController.java
+++ src/main/java/com/ohgiraffers/notimplement/user/controller/UserController.java
@@ -1,13 +1,37 @@
 package com.ohgiraffers.notimplement.user.controller;
 
+import com.ohgiraffers.notimplement.user.model.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller("/user")
+@Controller
+@RequestMapping("/user")
 public class UserController {
 
-    @GetMapping("/main")
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/info")
     public String main() {
-        return "userPage/main";
+        return "userPage/userInfo";
+    }
+
+
+    @GetMapping("/point")
+    public String point(Model model) {
+
+        int userId = 1;
+
+        // 데이터 베이스에서 고객의 사용 가능한 적립금 계산해서 반환
+        int availablePoint = userService.getAvailablePoint(userId);
+
+        model.addAttribute("availablePoint", availablePoint);
+        return "userPage/point/point";
     }
 }
