79a074c0660852040bff6d9015bceb9713c32c64
Rohrok <yr951103@gmail.com>
2024-06-25 21:40:46 +0900

    feature : 관리자 상품 목록 구현
    - 전체 상품 목록 표시
    - 상품 10개씩 목록 표시
    - 페이징 처리
    
    resolves : #4

--- src/main/java/com/ohgiraffers/notimplement/product/service/ProductService.java
+++ src/main/java/com/ohgiraffers/notimplement/product/service/ProductService.java
@@ -1,8 +1,11 @@
 package com.ohgiraffers.notimplement.product.service;
 
 import com.ohgiraffers.notimplement.product.model.dao.ProductMapper;
+import com.ohgiraffers.notimplement.product.model.domain.Product;
 import com.ohgiraffers.notimplement.product.model.dto.DashboardResponse;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductService {
@@ -17,5 +20,9 @@ public class ProductService {
         int soldOutProductCount = productMapper.soldOutProductCount();
 
         return new DashboardResponse(allProductCount, soldOutProductCount);
+    }
+
+    public List<Product> findAllProduct() {
+        return productMapper.findAllProduct();
     }
 }
