1e64c6cc78d73fc8a1d8138a82c15105d4f827ca
jisoo78 <ekelldks@naver.com>
2024-06-27 19:34:15 +0900

    Feat : 주문서 작성 기능 구현
    DB 에서 구매자 이름과 배송지, 휴대번호를 불러올 수 있음
    상품 시퀀스를 이용하여 렌탈기간 금액 불러 오고 주문 제품 테이블에 저장을 할 수 있음
    
    Resolve : #24

--- src/main/java/com/ohgiraffers/notimplement/order/controller/OrderController.java
+++ src/main/java/com/ohgiraffers/notimplement/order/controller/OrderController.java
@@ -1,31 +1,35 @@
 package com.ohgiraffers.notimplement.order.controller;
 
+import com.ohgiraffers.notimplement.auth.model.dto.UserDTO;
 import com.ohgiraffers.notimplement.order.model.dto.DetailOrderDTO;
 import com.ohgiraffers.notimplement.order.model.dto.OrderDTO;
 import com.ohgiraffers.notimplement.order.model.dto.OrderDeliveryDTO;
+import com.ohgiraffers.notimplement.order.model.dto.ProcessOrderDTO;
 import com.ohgiraffers.notimplement.order.model.service.OrderService;
-import com.ohgiraffers.notimplement.user.controller.UserController;
+import com.ohgiraffers.notimplement.product.model.dto.ProductResponse;
+import com.ohgiraffers.notimplement.product.service.ProductService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.http.HttpRequest;
 import java.util.List;
 
 @Controller
 @Slf4j
 @RequestMapping("/order")
+@RequiredArgsConstructor
 public class OrderController {
 
     private final OrderService orderService;
-    private final UserController user;
-
-    public OrderController(OrderService orderService, UserController user) {
-        this.orderService = orderService;
-        this.user = user;
-    }
+    private final ProductService productService;
 
     @GetMapping("/infoLookup")
     public String findOrderList(Model model) {
@@ -47,14 +51,86 @@ public class OrderController {
     // 구매자 주문배송조회
     @GetMapping("/orderDelivery")
     public String orderDelivery(HttpServletRequest request, Model model) {
+
         HttpSession session = request.getSession();
         String userId = (String) session.getAttribute("id");
 
-        System.out.println("userId = " + userId);
         List<OrderDeliveryDTO> orderDeliveryList = orderService.findAllOrderDelivery(userId);
         model.addAttribute("orderDeliveryList", orderDeliveryList);
 
-
         return "userPage/order/orderDeliveryLookup";
     }
+
+    // 구매자 주문서 작성
+    @GetMapping("/orderProcess")
+    public String placeOrder(@RequestParam("productSequence") int productSequence,
+                             @RequestParam("month") int month, Model model, HttpServletRequest request) {
+
+        HttpSession session = request.getSession();
+        String userId = (String) session.getAttribute("id");
+
+        UserDTO user = orderService.getUserData(userId);
+
+        model.addAttribute("productSequence", productSequence);
+        model.addAttribute("user", user);
+
+        // 1.find product
+//        ProcessOrderDTO processOrderList = productService.findAllOrderProcess(productSequence);
+//        if (processOrderList == null) {
+//
+//            return "userPage/product/main";
+//        }
+
+
+        ProductResponse productById = productService.getById(productSequence);
+
+        model.addAttribute("productById", productById);
+
+
+        // 2. update price
+//        int updatedPrice = processOrderList.getProductPrice() * month;
+
+        int newPrice = productById.price() * month;
+        model.addAttribute("newPrice", newPrice);
+
+        // 3. make order
+        ProcessOrderDTO processOrderList = new ProcessOrderDTO();
+
+        // 4. save order
+//        List<ProcessOrderDTO> processOrderList =
+        orderService.save(productSequence, newPrice);
+
+//        model.addAttribute("processOrderList", processOrderList);
+
+        return "userPage/order/processOrder";
+    }
+
+    @PostMapping("/sendOrder")
+    @Transactional(isolation = Isolation.DEFAULT, propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+    @ResponseBody
+    public String sendOrder(@RequestParam String productSeq, @RequestParam String productPrice, HttpServletRequest request) {
+
+        HttpSession session = request.getSession();
+        String userId = (String) session.getAttribute("id");
+
+        orderService.sendOrder(productSeq, productPrice, userId);
+
+        return "주문에 성공하였습니다.";
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
